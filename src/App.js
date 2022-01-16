1c7d5b66566c536cbeaa13f3d9675e02fbeadb2c
Felicia <nemet.felicia@gmail.com>
2022-01-16 15:50:18 -0500

    added select menu

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import './App.css';
 
 function App() {
   const [character, setCharacter] = useState([]);
+  const [userInput, setUserInput] = useState('');
+  const [searchTerm, setSearchTerm] = useState('')
 
   useEffect( () => {
     
@@ -15,20 +17,52 @@ function App() {
       method: 'GET',
       dataResponse: 'json',
       params: {
-        // q: 'Spider-Man'
-        nameStartsWith: 'Iron Man',
+        name: "Iron Man",
         limit: 1
       }
     }).then((response) => {
       console.log(response.data.data.results)
-      setCharacter(response)
+      setCharacter(response.data.data.results)
     });
-  }, []);
+  }, [searchTerm]);
+
+  const handleInput = (event) => {
+    console.log('is this working?', event.target.value);
+    setCharacter(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearchTerm(character);
+  }
+
 
 
   return (
     <div className="App">
       <h1>Choose Your Character</h1>
+      <h2>Please select a character below.</h2>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="characterChoice">Which character?</label>
+        <select name="characterChoice" id="characterChoice">
+          <option value defaultValue >Please choose a character</option>
+          <option value="spider-man">Spider-Man</option>
+          <option value="iron man">Iron Man</option>
+          <option value="green- oblin">Green Goblin</option>
+          <option value="doctor octopus">Doctor Octopus</option>
+          <option value="daredevil">Daredevil</option>
+          <option value="iron fist">Iron Fist</option>
+          <option value="venom eddie brock">Venom</option>
+          <option value="black panther">Black Panther</option>
+          <option value="erik killmonger">Erik Killmonger</option>
+        </select>
+      <button>Submit</button>
+      </form>
+      <div>
+        <h3>{}</h3>
+        <img src="" alt="" />
+      </div>
     </div>
   );
 }
