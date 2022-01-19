2612ee6e66542ec8d3e14b906ebe0fee67fb4a0f
Felicia <nemet.felicia@gmail.com>
2022-01-19 18:15:32 -0500

    updated url

--- src/utils/apiCall.js
+++ src/utils/apiCall.js
@@ -8,7 +8,7 @@ const insert = (parameter) => {
 const apiCall = (characterId = ``) => {
      
 return axios({
-     url: `http://gateway.marvel.com/v1/public/characters${insert(characterId)}?ts=1&apikey=a8e885bb92f8b3282a281b15ee8b4a19&hash=a0492b78fb5d29118e5a7fda370cbcd4`,
+     url: `https://gateway.marvel.com/v1/public/characters${insert(characterId)}?ts=1&apikey=a8e885bb92f8b3282a281b15ee8b4a19&hash=a0492b78fb5d29118e5a7fda370cbcd4`,
      method: 'GET',
      dataResponse: 'json',
      }).then((response) => {
